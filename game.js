8fd2d7524a23d85f70edffa6767643d06d032a26
hairvader <hairvader@gmail.com>
2015-12-14 17:58:59 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,31 @@
+// Check if the user is ready to play!
+confirm("I want to play breh");
+
+var age = prompt("what's your age");
+
+if( age < 13)
+{
+    console.log("You're allowed to play but I take no responsibility, pls don't sue me");
+}
+else {
+    console.log("Congrats you're old enough to play dog");
+}
+confirm("pls look at the console to the right, not the gaming one you dingus, thx");
+console.log("You're at a csgo lan and you beat some dudes, they get angry and try to beat the shit out of you with doriots");
+
+console.log("You're like 'breh wtf' so you grab your shit");
+
+var userAnswer = prompt("Do you want run the hell away");
+if (userAnswer === "yes") {
+    console.log("You make it out of the LAN venue alive with only minor cheesy fingers");
+}
+else {
+    console.log("You are absolutlely destroyed with doriots and convered in cheese and other shit");
+}
+var feedback = prompt("rate my shit out of 10 breh");
+if ( feedback > 8 ) {
+    console.log("Thx breh,this should help you if you come into this situation");
+}
+else {
+    console.logg("Breh if this wasn't a 10 outta 10 i don't know what is, no refunds breh hahaha");
+}
